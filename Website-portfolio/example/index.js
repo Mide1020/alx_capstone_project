c5c2d25f83667cff0b354ae376cdac97e4b77b2c
mide1020 <akinrefonayomide1@gmail.com>
2023-11-18 19:50:54 +0100

    My portfolio

---
+++ Website-portfolio/example/index.js
@@ -0,0 +1,9 @@
+let class1 = ["motun", "dammy", "ezzy"];
+let class2 = ["simon", "ekpa", "moyin"];
+let class3 = ["moji", "tinu", "tiara"];
+
+let students = [class1, class2, class3];
+
+for(let list of students);
+   for(let people of list);
+console.log(people);
